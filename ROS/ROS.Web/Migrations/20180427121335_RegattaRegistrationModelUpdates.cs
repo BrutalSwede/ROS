7dc16f92469158a069ce0c2ffd584263ea2fb5a7
PM <pontus.bua@gmail.com>
2018-04-27 14:27:33 +0200

    Created registration form for regattas

---
+++ ROS/ROS.Web/Migrations/20180427121335_RegattaRegistrationModelUpdates.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ROS.Web.Migrations
+{
+    public partial class RegattaRegistrationModelUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Message",
+                table: "RegattaRegistration",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Message",
+                table: "RegattaRegistration");
+        }
+    }
+}
