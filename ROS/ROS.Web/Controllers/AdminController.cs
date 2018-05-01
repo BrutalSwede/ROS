32cce4dd275041496fc4fe576254e92ca33ed500
Joakim <jkarlkvist@gmail.com>
2018-05-01 22:29:34 +0200

    More Admin stuff (see desc)
    Added GET's and Views for Users, Boats, Clubs and Regattas for admin. Also added Admin-button in Navbar if the user logged in has admin role. 
    Will continue adding functionality for admins such as Edit-functions and Delete-functions for all Admin actions

--- ROS/ROS.Web/Controllers/AdminController.cs
+++ ROS/ROS.Web/Controllers/AdminController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ROS.Web.Data;
+using ROS.Web.Models;
 using ROS.Web.Models.AdminViewModels;
 
 namespace ROS.Web.Controllers
@@ -14,20 +16,60 @@ namespace ROS.Web.Controllers
     public class AdminController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AdminController(ApplicationDbContext context)
+        public AdminController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
         {
             _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
 
-        // GET Boats
-        public async Task<IActionResult> Index()
+        // GET /<controller>/
+        public IActionResult Index()
         {
-            var boats = await _context.Boats.ToListAsync();
-
-            var BoatVm = new AdminBoatViewModel { Boats = boats };
-
-            return View(BoatVm);
+            return View();
         }
+
+        // GET all users
+        public IActionResult UserManagement()
+        {
+            var users = _userManager.Users;
+
+            return View(users);
+        }
+
+        // GET all boats
+        public async Task<IActionResult> BoatManagement()
+        {
+            var boats = await _context.Boats.Include(o => o.Owner).ToListAsync();
+            
+            return View(boats);
+        }
+       
+        // GET all clubs
+        public async Task<IActionResult> ClubManagement()
+        {
+            var clubs = await _context.Clubs.Include(o => o.Owner).ToListAsync();
+
+            return View(clubs);
+        }
+
+        // GET all regattas
+        public async Task<IActionResult> RegattaManagement()
+        {
+            var regattas = await _context.Regattas.Include(o => o.CreatedBy).ToListAsync();
+
+            return View(regattas);
+        }
+        
+
+        //TODO: Edit and Delete User, Boat, Club, Regatta
+
+
     }
 }
