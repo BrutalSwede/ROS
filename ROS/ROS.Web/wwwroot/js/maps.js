e5f7e63a9d33835211685d708fb0384547786b71
PM <pontus.bua@gmail.com>
2018-05-03 15:43:32 +0200

    End of day push
    Not quite finished with the Autocomplete input. Still not getting the Api to load properly from Google, but I'll try to get it working tonight.

--- ROS/ROS.Web/wwwroot/js/maps.js
+++ ROS/ROS.Web/wwwroot/js/maps.js
@@ -10,6 +10,40 @@ function initialize() {
         center: latlng
     }
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    // Enable autocomplete
+    var input = document.getElementById('Address');
+
+    var autocomplete = google.maps.places.Autocomplete(input);
+
+    autocomplete.addListener('place_changed', function () {
+        if (marker) {
+            marker.setVisible(false);
+        } 
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            window.alert("No details for " + place.name + "!");
+            return;
+        }
+    });
+
+    if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+    } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);  // Why 17? Because it looks good.
+    }
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+        address = [
+            (place.address_components[0] && place.address_components[0].short_name || ''),
+            (place.address_components[1] && place.address_components[1].short_name || ''),
+            (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+    }
 }
 
 function codeAddress() {
