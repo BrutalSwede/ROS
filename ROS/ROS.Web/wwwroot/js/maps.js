e41f2fae86ce9509be09b818e36672bc30ed2e32
PM <pontus.bua@gmail.com>
2018-05-04 09:46:50 +0200

    Implement Google Places Autocomplete

--- ROS/ROS.Web/wwwroot/js/maps.js
+++ ROS/ROS.Web/wwwroot/js/maps.js
@@ -13,37 +13,12 @@ function initialize() {
 
     // Enable autocomplete
     var input = document.getElementById('Address');
+    var options = {
+        types: [],
+        componentRestrictions: {country: 'se'}
+    };
 
-    var autocomplete = google.maps.places.Autocomplete(input);
-
-    autocomplete.addListener('place_changed', function () {
-        if (marker) {
-            marker.setVisible(false);
-        } 
-        var place = autocomplete.getPlace();
-        if (!place.geometry) {
-            window.alert("No details for " + place.name + "!");
-            return;
-        }
-    });
-
-    if (place.geometry.viewport) {
-        map.fitBounds(place.geometry.viewport);
-    } else {
-        map.setCenter(place.geometry.location);
-        map.setZoom(17);  // Why 17? Because it looks good.
-    }
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
-
-    var address = '';
-    if (place.address_components) {
-        address = [
-            (place.address_components[0] && place.address_components[0].short_name || ''),
-            (place.address_components[1] && place.address_components[1].short_name || ''),
-            (place.address_components[2] && place.address_components[2].short_name || '')
-        ].join(' ');
-    }
+    var autocomplete = new google.maps.places.Autocomplete(input, options);
 }
 
 function codeAddress() {
@@ -51,6 +26,7 @@ function codeAddress() {
     geocoder.geocode({ 'address': address }, function (results, status) {
         if (status == 'OK') {
             map.setCenter(results[0].geometry.location);
+            map.setZoom(15);
 
             if (marker) {
                 marker.setPosition(results[0].geometry.location);
