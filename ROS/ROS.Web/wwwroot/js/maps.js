b9dd4c00f1792422181fb96304fa39905fa1265a
PM <pontus.bua@gmail.com>
2018-05-04 10:01:32 +0200

    Added override on 'Enter' key in form

--- ROS/ROS.Web/wwwroot/js/maps.js
+++ ROS/ROS.Web/wwwroot/js/maps.js
@@ -15,10 +15,21 @@ function initialize() {
     var input = document.getElementById('Address');
     var options = {
         types: [],
-        componentRestrictions: {country: 'se'}
+        componentRestrictions: { country: 'se' } // only suggest places in Sweden
     };
 
     var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+    // Override the form submit when you press 'Enter' whilst in the 'Address' input field
+    input.addEventListener('keydown', function (e) {
+
+        var key = e.which || e.key;
+
+        if (key == 13) { // Key# 13 is the 'Enter' key
+            e.preventDefault();
+            codeAddress();
+        }
+    });
 }
 
 function codeAddress() {
@@ -28,6 +39,7 @@ function codeAddress() {
             map.setCenter(results[0].geometry.location);
             map.setZoom(15);
 
+            // Only allow one marker to be on the map at any one time
             if (marker) {
                 marker.setPosition(results[0].geometry.location);
             }
