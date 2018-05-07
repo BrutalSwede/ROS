5b7be2e42754238c40d9ed9cbde1c9ca6fa112bd
Joakim <jkarlkvist@gmail.com>
2018-05-07 20:26:01 +0200

    Minor changes to club list layout and club profile layout. Not final

--- ROS/ROS.Web/wwwroot/js/site.js
+++ ROS/ROS.Web/wwwroot/js/site.js
@@ -1,1 +1,13 @@
 ﻿// Write your JavaScript code.
+$(document).on('click', '.panel-heading span.clickable', function(e){
+    var $this = $(this);
+	if(!$this.hasClass('panel-collapsed')) {
+		$this.parents('.panel').find('.specialCollapse').slideUp();
+		$this.addClass('panel-collapsed');
+		$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+	} else {
+		$this.parents('.panel').find('.specialCollapse').slideDown();
+		$this.removeClass('panel-collapsed');
+		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+	}
+})
