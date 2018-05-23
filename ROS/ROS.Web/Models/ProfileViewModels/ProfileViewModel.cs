f488f96440e4bee016be468bbf09e9e7ad2d2214
Unknown <ginokalyun@gmail.com>
2018-05-23 13:11:35 +0200

    Added User-Profile page
    Merged Development to User_Profile and then auto stashed and fixed rest

---
+++ ROS/ROS.Web/Models/ProfileViewModels/ProfileViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ROS.Web.Models.ManageViewModels
+{
+    public class ProfileViewModel
+    {
+        [DisplayName("Förnamn")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Efternamn")]
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public int AmountOfBoats { get; set; }
+
+        [Phone]
+        [DisplayName("Telefonnummer")]
+        public string Phone { get; set; }
+
+        [Phone]
+        [DisplayName("ICE Telefonnummer")]
+        public string IcePhone { get; set; }
+
+        public IList<ClubApplication> Applications { get; set; }
+        public IList<Boat> Boats { get; set; }
+    }
+}
